5b990e9e78070484d86e4b6786e8bbf2d1d18a13
Gabey <fluffygabey@gmail.com>
2022-12-11 17:18:22 +0100

    debut script init db (a completer)

---
+++ Quickstart/initDatabase.sql
@@ -0,0 +1,4 @@
+-- Création des différentes base de donnés
+CREATE DATABASE IF NOT EXIST gestAgricoleProd;
+CREATE DATABASE IF NOT EXIST gestAgricoleDev;
+CREATE DATABASE IF NOT EXIST gestAgricoleTest;
