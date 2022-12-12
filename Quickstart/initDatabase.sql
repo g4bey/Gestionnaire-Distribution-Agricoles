871bc936f5bfc0dd68456823cb9ab55d58e4ce7f
Gabey <fluffygabey@gmail.com>
2022-12-12 03:50:11 +0100

    init base de test.

--- Quickstart/initDatabase.sql
+++ Quickstart/initDatabase.sql
@@ -2,3 +2,16 @@
 CREATE DATABASE IF NOT EXIST gestAgricoleProd;
 CREATE DATABASE IF NOT EXIST gestAgricoleDev;
 CREATE DATABASE IF NOT EXIST gestAgricoleTest;
+
+--
+-- Création de la base de test
+--
+USE gestAgricoleTest;
+CREATE TABLE `users` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `username` varchar(256) NOT NULL,
+    `email` varchar(355) NOT NULL,
+    `password` varchar(256) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
