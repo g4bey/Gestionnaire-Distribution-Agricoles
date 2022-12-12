008d02c641275380a0cf4f05a233435d099db8d1
Gabey <fluffygabey@gmail.com>
2022-12-12 02:29:34 +0100

    Documentation test connecteur

--- src/Testing/Database/DatabaseConnectionTest.java
+++ src/Testing/Database/DatabaseConnectionTest.java
@@ -9,17 +9,28 @@ import java.sql.SQLException;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DatabaseConnectionTest {
+
+    /**
+     * Nous vérifions que le singleton renvoie bien une instance lors que
+     * getInstance est invoqué pour la premiere fois.
+     */
     @Test
     @DisplayName("Premiere connection valide")
     void connectionValide() throws IOException, ClassNotFoundException {
         try {
-            Connection conn = DatabaseConnection.getInstance("production");
+            DatabaseConnection.close(DatabaseConnection.getInstance("testing"));
+            Connection conn = DatabaseConnection.getInstance("testing");
             assertNotNull(conn);
             DatabaseConnection.close(conn);
         } catch (SQLException e) {
             fail("Serveur MySQL indisponible");
         }
     }
+
+    /**
+     * Nous vérifions qu'un environnement inexistant du fichier de configuration
+     * génère bien une erreur.
+     */
     @Test
     @DisplayName("Chargeur d'attribut fonctionnel")
     void chargeurAttributFonctionnel() throws IOException, ClassNotFoundException {
@@ -32,14 +43,19 @@ public class DatabaseConnectionTest {
             assertNull(conn);
         }
     }
+
+    /**
+     * Nous cherchons à tester l'unicité de la connection.
+     * Le singleton doit renvoyer une instance si elle existe deja.
+     */
     @Test
     @DisplayName("Singleton retournant une et une seule connection")
     void singletonFonctionnel() throws IOException, ClassNotFoundException {
         Connection conn;
         Connection conn2;
         try {
-            conn = DatabaseConnection.getInstance("production");
-            conn2 = DatabaseConnection.getInstance("production");
+            conn = DatabaseConnection.getInstance("testing");
+            conn2 = DatabaseConnection.getInstance("testing");
             assertEquals(conn, conn2);
             DatabaseConnection.close(conn);
             DatabaseConnection.close(conn2);
