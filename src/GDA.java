1eafbe0271d69172281f9be7631bab42530d7d62
Romain9 <romain.msiah@gmail.com>
2022-12-19 17:42:16 +0100

    Lien entre contrôleur et vues
    Début de liaison entre contrôleur et vues, navigation disponible pour les 3 premières vues.

--- src/GDA.java
+++ src/GDA.java
@@ -1,8 +1,35 @@
-public class GDA {
+package src;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import src.controllers.HomePageCtrl;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+
+public class GDA extends Application {
     /**
      * @param args
      */
 
-    public static void main(String args) {
+    public static void main(String[] args) {
+    	launch(args);
     }
+    
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+    	Parent root = FXMLLoader.load(getClass().getResource("views/homePage.fxml"));
+    	Scene scene = new Scene(root);
+    	Image icon = new Image("src/ressources/pictures/1751148-ff5722.png");
+    	
+    	primaryStage.setTitle("Gestionnaire-Distribution-Agricoles");
+    	primaryStage.setResizable(false);
+    	primaryStage.getIcons().add(icon);
+    	primaryStage.setScene(scene);
+    	primaryStage.show();
+    }
+    
 }
--- src/controllers/ProdConnCtrl.java
+++ src/controllers/ProdConnCtrl.java
@@ -1,10 +1,43 @@
-package controllers;
+package src.controllers;
 
-
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 /**
 * Contrôleur permettant la connexion d'un Producteur.
 */
 
 public class ProdConnCtrl {
+    @FXML
+	private TextField prodSiretField;
+	
+    @FXML
+	private TextField prodPasswordField;
+
+    @FXML
+    private Button validateProdConn;
+    
+    @FXML
+    private Button cancelProdConn;
+    
+    @FXML
+    private Text connErreurText;
+    
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    
+    /**
+     * Méthode pour connecter l'utilisateur.
+     * @param event
+     */
+    public void loginAdmin(ActionEvent event) {
+    	
+    }
 }
