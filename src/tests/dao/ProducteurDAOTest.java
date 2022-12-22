6fd6b64ebb2873734fa229eafd2bbdaba6b56d52
Gabriel <fluffygabey@gmail.com>
2022-12-22 01:01:49 +0100

    Test propagation producteur

--- src/tests/dao/ProducteurDAOTest.java
+++ src/tests/dao/ProducteurDAOTest.java
@@ -1,8 +1,7 @@
 package tests.dao;
 
-import DAO.ProducteurDAO;
-import modele.Producteur;
-import modele.Vehicule;
+import DAO.*;
+import modele.*;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,15 +9,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import utility.DatabaseConnection;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Timestamp;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Tests de la classe DAO.prodDAO.
@@ -87,7 +85,7 @@ public class ProducteurDAOTest {
         PRODUCTEUR_B = new Producteur("73282932000075", "Patrick", "13 rue du Poulpe", "0612934563",
                 "48.37760650.8082932", "hashMDP2");
 
-        VEHICULE_A = new Vehicule("AA229AA", 32, "véhicule principal", PRODUCTEUR_A);
+        VEHICULE_A = new Vehicule("AA229AA", 32F, "véhicule principal", PRODUCTEUR_A);
 
         truncateTable("Producteur");
         truncateTable("Vehicule");
@@ -186,7 +184,7 @@ public class ProducteurDAOTest {
     @Test
     @DisplayName("Test la methode update")
     public void updateTest() {
-        PRODUCTEUR_A.addVehicule(VEHICULE_B);
+        PRODUCTEUR_A.addVehicule(VEHICULE_A);
 
         // On ajoute puis met à jour le PRODUCTEUR_A
         // L'on change aussi le producteur
@@ -203,5 +201,76 @@ public class ProducteurDAOTest {
         // sont identiques. Cela induit qu'ils sont modifés en BDD.
         Producteur ProducteurRetour = prodDAO.get(PRODUCTEUR_A.getIdProducteur());
         assertTrue(ProducteurRetour.equals(PRODUCTEUR_A));
+    }
+
+    /**
+     * On s'assure que les références vers d'autres objets sont
+     * bien mises à jour.
+     */
+    @Test
+    @DisplayName("Test de la propagation")
+    public void propagationTest() {
+        prodDAO.add(PRODUCTEUR_A);
+
+        // On ajoute un client en base
+        ClientDAO clientDAO = new ClientDAO(conn);
+        Client client = new Client("Jean", "120 Rue", "123,987", "229938");
+        clientDAO.add(client);
+
+        // On ajoute une commande en base
+        CommandeDAO commandeDAO = new CommandeDAO(conn);
+        Commande commande = new Commande(
+                "Com", 20F, new Timestamp(10000), new Timestamp(20000),
+                PRODUCTEUR_A, client
+        );
+        commandeDAO.add(commande);
+
+        // On verifie que le producteur possede bien cette commande.
+        assertTrue(PRODUCTEUR_A.getCommandes().get(0).equals(commande));
+
+        // On insère une autre commande par le producteur.
+        Commande commandeBis = new Commande(
+                "Com2", 20F, new Timestamp(100000), new Timestamp(200000),
+                PRODUCTEUR_A, client
+        );
+        PRODUCTEUR_A.addCommande(commandeBis);
+        prodDAO.update(PRODUCTEUR_A);
+
+        // On constate que c'est bien dans l'objet.
+        assertTrue(PRODUCTEUR_A.getCommandes().get(1).equals(commandeBis));
+
+        // On creer un nouveau vehicule
+        Vehicule vehicule = new Vehicule("9897-FF", 90F, "Vehi", PRODUCTEUR_A);
+        VehiculeDAO vehiDAO = new VehiculeDAO(conn);
+        vehiDAO.add(vehicule);
+
+        // On ajoute le vehicule au producteur.
+        PRODUCTEUR_A.addVehicule(vehicule);
+        prodDAO.update(PRODUCTEUR_A);
+
+        // On s'assure qu'il possède bien ce vehicle.
+        assertTrue(PRODUCTEUR_A.getVehicules().contains(vehicule));
+
+        // On creer une nouvelle tournee
+        Tournee tournee = new Tournee(new Timestamp(100000), new Timestamp(100000), 30F, "Tournee", vehicule);
+        TourneeDAO tourneeDAO = new TourneeDAO(conn);
+        tourneeDAO.add(tournee);
+
+        // On verifie que le producteur a bien cette tournee.
+        assertTrue(PRODUCTEUR_A.getTournees().contains(tournee));
+
+        // On change de vehicule pour la tournee.
+        vehiDAO.add(VEHICULE_A);
+        tournee.setVehicule(VEHICULE_A);
+        tourneeDAO.update(tournee);
+
+        // On constate que c'est update dans le tableau de tournee.
+        assertTrue(PRODUCTEUR_A.getTournees().get(0).getVehicule().equals(VEHICULE_A));
+
+        // On supprime la tournee, et constate qu'elle n'est plus
+        // dans le tableau de tournee du producteur.
+        tourneeDAO.delete(tournee);
+        assertFalse(PRODUCTEUR_A.getTournees().contains(tournee));
+
     }
 }
