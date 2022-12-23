755c2dc4d2c1cbb21e6964264144f8956e046e6a
Romain9 <romain.msiah@gmail.com>
2022-12-23 20:23:20 +0100

    Style change

--- src/GDA.java
+++ src/GDA.java
@@ -21,7 +21,7 @@ public class GDA extends Application {
     public void start(Stage primaryStage) throws IOException {
     	Parent root = FXMLLoader.load(getClass().getResource("/views/homePage.fxml"));
     	Scene scene = new Scene(root);
-    	Image icon = new Image("/ressources/pictures/p&sLogo.png");
+    	Image icon = new Image("ressources/pictures/Logo.png");
     	
     	primaryStage.setTitle("Gestionnaire-Distribution-Agricoles");
     	primaryStage.setResizable(false);
