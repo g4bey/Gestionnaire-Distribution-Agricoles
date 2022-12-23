956a89dbc05638792862c8d3d15fa675f7a20165
Gabriel <fluffygabey@gmail.com>
2022-12-23 21:48:39 +0100

    Precision AdresseInvalideException

--- src/exceptions/AdresseInvalideException.java
+++ src/exceptions/AdresseInvalideException.java
@@ -1,5 +1,8 @@
 package exceptions;
 
+/**
+ * L'adresse postale n'est pas valide.
+ */
 public class AdresseInvalideException extends Exception {
     public AdresseInvalideException(String error) {
         super(error);
