2845cfac96afa313e7320286eba0987927d8693e
Alpha-Lin <alpha_lin@protonmail.com>
2022-12-27 19:22:46 +0100

    Made AdminSelectMenuCtrl

--- src/controllers/AbstractConnCtrl.java
+++ src/controllers/AbstractConnCtrl.java
@@ -5,11 +5,15 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 import DAO.AdministrateurDAO;
+import DAO.ClientDAO;
+import DAO.ProducteurDAO;
 import utility.DatabaseConnection;
 
 public abstract class AbstractConnCtrl {
     protected Connection conn = null;
     protected AdministrateurDAO aDAO = null;
+    protected ProducteurDAO pDAO = null;
+    protected ClientDAO cltDAO = null;
 
     public AbstractConnCtrl() {
         try {
@@ -20,5 +24,7 @@ public abstract class AbstractConnCtrl {
         }
 
         aDAO = new AdministrateurDAO(conn);
+        pDAO = new ProducteurDAO(conn);
+        cltDAO = new ClientDAO(conn);
     }
 }
