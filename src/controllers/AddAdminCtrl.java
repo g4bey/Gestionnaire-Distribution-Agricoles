e6c548cc8ca5535cc256956015e8fb17090f6b72
Pipoune <GnQ@outlook.fr>
2022-12-27 20:35:30 +0100

    Update AddAdminCtrl

--- src/controllers/AddAdminCtrl.java
+++ src/controllers/AddAdminCtrl.java
@@ -4,16 +4,20 @@ import de.mkammerer.argon2.Argon2;
 import de.mkammerer.argon2.Argon2Factory;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import modele.Administrateur;
 import utility.ControllersUtils;
 import validForm.FormAdminAddValidator;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
 * Contrôleur permettant l'ajout d'un Administrateur.
 */
-public class AddAdminCtrl extends AbstractConnCtrl {
+public class AddAdminCtrl extends AbstractConnCtrl implements Initializable {
 	
     @FXML
 	private TextField adminLoginField;
@@ -26,18 +30,23 @@ public class AddAdminCtrl extends AbstractConnCtrl {
     
     @FXML
     private Text formErrorText;
-	
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        formErrorText.setVisible(false);
+    }
+
 	/**
 	* Méthode qui valide l'ajout d'un administrateur.
 	* @param event ActionEvent
 	*/
     public void validateAddAdmin(ActionEvent event) {
-        formErrorText.setVisible(false);
         FormAdminAddValidator faav = new FormAdminAddValidator(
             adminLoginField.getText(),
             adminPasswordField.getText(),
             confirmPasswordField.getText());
         if (faav.isValid()) {
+            formErrorText.setVisible(false);
             Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id, 32, 64);
             String hashedPs = argon2.hash(2,15*1024,1, adminPasswordField.getText().toCharArray());
             aDAO.add(new Administrateur(adminLoginField.getText(), hashedPs));
