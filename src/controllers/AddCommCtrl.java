676db8226c38ca991c1a415473c8efb19f998049
Pipoune <GnQ@outlook.fr>
2022-12-27 20:35:55 +0100

    Update AddCommCtrl

--- src/controllers/AddCommCtrl.java
+++ src/controllers/AddCommCtrl.java
@@ -1,7 +1,10 @@
 package controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
@@ -9,10 +12,13 @@ import javafx.scene.text.Text;
 import modele.Client;
 import utility.ControllersUtils;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
 * Contrôleur permettant l'ajout d'une Commande.
 */
-public class AddCommCtrl {
+public class AddCommCtrl extends AbstractConnCtrl implements Initializable {
 	
     @FXML
     private TextField commLabelField;
@@ -34,7 +40,15 @@ public class AddCommCtrl {
     
     @FXML
     private Text formErrorText;
-    
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        formErrorText.setVisible(false);
+        ObservableList<Client> listeClients = FXCollections.observableArrayList();
+        listeClients.addAll(cltDAO.getAll());
+        clientChoiceBox.setItems(listeClients);
+    }
+
     /**
     * Méthode qui valide l'ajout d'une commande.
     * @param event ActionEvent
