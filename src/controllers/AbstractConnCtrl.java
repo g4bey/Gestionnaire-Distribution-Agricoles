280654251539dadfb5a5f867b355eb4dac5171b9
Gabriel <fluffygabey@gmail.com>
2022-12-28 15:17:24 +0100

    Instanciation des DAO statique.

--- src/controllers/AbstractConnCtrl.java
+++ src/controllers/AbstractConnCtrl.java
@@ -12,17 +12,32 @@ import DAO.TourneeDAO;
 import DAO.VehiculeDAO;
 import utility.DatabaseConnection;
 
+/**
+ * Les controllers nécessitant une connexion à la BDD dérivent de ce controleur.
+ * On n'instancie les DAO qu'une seule fois, ainsi ces attributs sont statiques.
+ */
 public abstract class AbstractConnCtrl {
-    protected Connection conn = null;
-    protected AdministrateurDAO aDAO = null;
-    protected ProducteurDAO pDAO = null;
-    protected ClientDAO cltDAO = null;
-    protected CommandeDAO commDAO = null;
-    protected TourneeDAO tDAO = null;
-    protected VehiculeDAO vDAO = null;
-    protected TourneeDAO tDAO = null;
+    protected static Connection conn = null;
+    protected static AdministrateurDAO aDAO = null;
+    protected static ProducteurDAO pDAO = null;
+    protected static ClientDAO cltDAO = null;
+    protected static CommandeDAO commDAO = null;
+    protected static TourneeDAO tDAO = null;
+    protected static VehiculeDAO vDAO = null;
 
+    /**
+     * On instancie les attributs que si c'est le premier appel d'une classe fille.
+     */
     public AbstractConnCtrl() {
+        if(conn == null) {
+            instantiateAttributes();
+        }
+    }
+
+    /**
+     * Instancie les DAO ainsi que la connection à la base donnée.
+     */
+    public void instantiateAttributes() {
         try {
             conn = DatabaseConnection.getInstance("production");
         } catch (ClassNotFoundException | SQLException | IOException e) {
