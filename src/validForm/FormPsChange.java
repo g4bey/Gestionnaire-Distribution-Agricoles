fcba382c0e46dbf97464a69f9644f196c7c4699f
Gabriel <fluffygabey@gmail.com>
2022-12-28 21:58:39 +0100

    Ajout char min changement de mdp

--- src/validForm/FormPsChange.java
+++ src/validForm/FormPsChange.java
@@ -1,7 +1,13 @@
 package validForm;
 
+import validator.ValidateurDonnee;
+
 public class FormPsChange extends  FormValidator {
     public FormPsChange(String ps, String comfirmPs) {
+        if(ValidateurDonnee.validePassword(ps)) {
+            setInvalid("Votre mots de passe doit faire au moins 8 caracteres.");
+        }
+
         if(!ps.equals(comfirmPs)) {
             setInvalid("Les mots de passe ne sont pas identique.");
         }
