f7053bcf3519629e88106947580bea082d067f72
Gabriel <fluffygabey@gmail.com>
2022-12-28 19:48:39 +0100

    AdminSelectMenu now updates live.

--- src/utility/ControllersUtils.java
+++ src/utility/ControllersUtils.java
@@ -1,8 +1,12 @@
 package utility;
 
+import java.awt.event.FocusEvent;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventTarget;
+import javafx.event.EventType;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -10,6 +14,7 @@ import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 /**
 * Classe utilitaire pour les contrôleurs.
@@ -20,7 +25,7 @@ public class ControllersUtils {
     private static Parent root;
     private static Stage stage;
     private static Scene scene;
-    
+
     /**
     * Méthode qui charge une vue passée en paramètre.
     * @param event MouseEvent
@@ -39,7 +44,7 @@ public class ControllersUtils {
     	stage.setScene(scene);
     	stage.show();
     }
-    
+
     /**
     * Méthode qui charge une vue passée en paramètre.
     * @param event MouseEvent
@@ -58,7 +63,7 @@ public class ControllersUtils {
     	stage.setScene(scene);
     	stage.show();
     }
-    
+
     /**
     * Méthode qui charge une vue popup passée en paramètre.
     * @param event ActionEvent
@@ -67,7 +72,7 @@ public class ControllersUtils {
     public void loadPopup(ActionEvent event, String path) {
     	try {
     	    root = FXMLLoader.load(getClass().getResource(path));
-        } 
+        }
         catch (IOException e) {
     			// TODO Auto-generated catch block
             e.printStackTrace();
@@ -80,7 +85,7 @@ public class ControllersUtils {
         stage.initModality(Modality.WINDOW_MODAL);
         stage.show();
     }
-    
+
     /**
     * Méthode qui charge une vue popup passée en paramètre.
     * @param event MouseEvent
@@ -89,7 +94,7 @@ public class ControllersUtils {
     public void loadPopup(MouseEvent event, String path) {
         try {
      	    root = FXMLLoader.load(getClass().getResource(path));
-        } 
+        }
         catch (IOException e) {
      			// TODO Auto-generated catch block
             e.printStackTrace();
@@ -102,13 +107,30 @@ public class ControllersUtils {
         stage.initModality(Modality.WINDOW_MODAL);
         stage.show();
     }
-	
+
 	/**
     * Méthode qui permet de fermer la vue popup.
     * @param event ActionEvent
     */
     public static void closePopup(ActionEvent event) {
     	stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        // On envoie la notification de fermeture de fenetre.
+        notifcationFermetureFenetre(stage);
     	stage.close();
+    }
+
+    /**
+     * Permet d'envoyer lors qu'on ferme une fenetre.
+     * On va notifier la fenetre ayant invoqué cette derniere.
+     * @param stage la fenetre qui provoque cette notification.
+     */
+    public static void notifcationFermetureFenetre(Stage stage) {
+        stage.getOwner().getOnCloseRequest().handle(
+                new WindowEvent(stage.getOwner(), WindowEvent.WINDOW_CLOSE_REQUEST)
+        );
+    }
+
+    public static Stage getStage() {
+        return stage;
     }
 }
