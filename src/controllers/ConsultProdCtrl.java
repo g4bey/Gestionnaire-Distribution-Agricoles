9b0ec4088b8f46e255c1fa5e6b03a433691d0f11
Alpha-Lin <alpha_lin@protonmail.com>
2022-12-28 15:36:15 +0100

    Made ConsultClientCtrl and ConsultProdCtrl

--- src/controllers/ConsultProdCtrl.java
+++ src/controllers/ConsultProdCtrl.java
@@ -1,7 +1,11 @@
 package controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.text.Text;
 import modele.Vehicule;
@@ -11,47 +15,60 @@ import modele.Commande;
 import modele.Producteur;
 
 /**
-* Contrôleur permettant l'aperçu Producteur.
-*/
-public class ConsultProdCtrl {
+ * Contrôleur permettant l'aperçu Producteur.
+ */
+public class ConsultProdCtrl implements Initializable {
 
     @FXML
     private Text prodSiretText;
-   
+
     @FXML
     private Text prodPropText;
-    
+
     @FXML
     private Text prodAddressText;
-    
+
     @FXML
     private Text prodPhoneText;
-    
+
     @FXML
     private ListView<Commande> prodCommListView;
-    
+
     @FXML
     private ListView<Tournee> prodTourListView;
-    
+
     @FXML
     private ListView<Vehicule> prodVehicleListView;
 
     private static Producteur producteur;
-    
-    /**
-    * Méthode qui permet de fermer la vue de consultation
-    * des informations d'un producteur.
-    * @param event ActionEvent
-    */
-    public void closeConsultProd(ActionEvent event) {
-    	ControllersUtils.closePopup(event);
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        prodSiretText.setText(producteur.getSiret());
+        prodPropText.setText(producteur.getProprietaire());
+        prodAddressText.setText(producteur.getAdresseProd());
+        prodPhoneText.setText(producteur.getNumTelProd());
+        prodCommListView.getItems().addAll(producteur.getCommandes());
+        prodTourListView.getItems().addAll(producteur.getTournees());
+        prodVehicleListView.getItems().addAll(producteur.getVehicules());
     }
 
     /**
-    * Méthode qui récupère le producteur sélectionné dans la listView
-    * de la vue précédente (adminSelectMenu)
-    * @param prod Producteur
-    */
+     * Méthode qui permet de fermer la vue de consultation
+     * des informations d'un producteur.
+     * 
+     * @param event ActionEvent
+     */
+    public void closeConsultProd(ActionEvent event) {
+        ControllersUtils.closePopup(event);
+    }
+
+    /**
+     * Méthode qui récupère le producteur sélectionné dans la listView
+     * de la vue précédente (adminSelectMenu)
+     * 
+     * @param prod Producteur
+     */
     public static void setProd(Producteur prod) {
         producteur = prod;
     }
