777164c9893f58ec40daefcb78096e6a7f6b60a5
Gabriel <fluffygabey@gmail.com>
2022-12-29 14:29:02 +0100

    Ajout doctstring FormProdAddValidator

--- src/validForm/FormProdAddValidator.java
+++ src/validForm/FormProdAddValidator.java
@@ -4,11 +4,28 @@ import exceptions.AdresseInvalideException;
 import validator.ValidateurAdresse;
 import validator.ValidateurDonnee;
 
+/**
+ * Utilisé lorsqu'on ajoute un producteur.
+ * @see controllers.AddProdCtrl
+ */
 public class FormProdAddValidator extends FormValidator {
 
     public String adresseCsv;
     public String coordsXY;
 
+    /**
+     * Constructeur de FormProdAddValidator.
+     * @param siret le siret du producteur.
+     * @param proprietaire le nom du producteur.
+     * @param addressNumField le numéro de rue du producteur.
+     * @param addressPathType le type de voie dans laquelle travaille le producteur.
+     * @param addressPathName le type de voie du Producteur récupéré dans la vue
+     * @param addressTownName la ville du Producteur.
+     * @param addressPostCode le code Postal du producteur
+     * @param numTelProd le numéro de téléphone du producteur.
+     * @param password le mot de passe du producteur
+     * @param confirmPassword le mot de passe à confirmer.
+     */
     public FormProdAddValidator(String siret,
                                 String proprietaire,
                                 String addressNumField,
