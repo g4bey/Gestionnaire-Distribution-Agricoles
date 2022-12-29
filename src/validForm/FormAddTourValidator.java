2ebe5a055ff8f94edbe9df8ef1f5557e4dd58594
Gabriel <fluffygabey@gmail.com>
2022-12-29 15:10:56 +0100

    Doctring Form Ajout Tournee + Fix

--- src/validForm/FormAddTourValidator.java
+++ src/validForm/FormAddTourValidator.java
@@ -11,11 +11,25 @@ import modele.Vehicule;
 import utility.DateManager;
 import validator.ValidateurTournee;
 
+/**
+ * Formulaire d'ajout de tournée.
+ * @see controllers.AddTourCtrl
+ */
 public class FormAddTourValidator extends FormValidator {
-    
+
+    /**
+     *
+     * @param libelle le libellé pour identifier la tournée.
+     * @param producteur le producteur associé à la tournée.
+     * @param vehicule le vehicle associé à la tournée.
+     * @param commandes la liste de commande composant la tournée.
+     * @param poids le poids de la tournée
+     * @param horaireDebut l'horaire de début de la tournée.
+     * @param horaireFin l'horaire de début de la tournée.
+     * @param date la date de la tournée.
+     */
     public FormAddTourValidator(String libelle, Producteur producteur, Vehicule vehicule, ArrayList<Commande> commandes, 
-    String poids, String horaireDebut, String horaireFin, String date) {
-        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    String poids, String horaireDebut, String horaireFin, LocalDate date) {
         if (libelle.isEmpty()) {
             setInvalid("Veuillez écrire un libellé !");
             return;
@@ -29,8 +43,8 @@ public class FormAddTourValidator extends FormValidator {
             return;
         }
         if (!ValidateurTournee.valideVehicule(vehicule, 
-        DateManager.convertToTimestamp(LocalDate.parse(date, format), horaireDebut), 
-        DateManager.convertToTimestamp(LocalDate.parse(date, format), horaireFin))) {
+        DateManager.convertToTimestamp(date, horaireDebut),
+        DateManager.convertToTimestamp(date, horaireFin))) {
             setInvalid("Le véhicule n'est pas disponible pour ce créneau horaire !");
         }
         if (!ValidateurTournee.validePoids(vehicule.getPoidsMax(), commandes)) {
