664c04a677c2429d2716c06dca2636ed0274e61e
Pipoune <GnQ@outlook.fr>
2023-01-05 15:55:50 +0100

    Correction orthographique et conventions de codage - FormAddTourValidator

--- src/validForm/FormAddTourValidator.java
+++ src/validForm/FormAddTourValidator.java
@@ -8,17 +8,20 @@ import modele.Vehicule;
 import validator.ValidateurTournee;
 
 /**
- * Formulaire d'ajout de tournée.
+ * Validateur du formulaire pour AddTourCtrl.
+ *
  * @see controllers.AddTourCtrl
  */
 public class FormAddTourValidator extends FormTourValidator {
+
     /**
+     * Constructeur de FormAddTourValidator
      *
-     * @param libelle    le libellé pour identifier la tournée.
-     * @param producteur le producteur associé à la tournée.
-     * @param vehicule   le vehicle associé à la tournée.
-     * @param commandes  la liste de commande composant la tournée.
-     * @param poids      le poids de la tournée
+     * @param libelle    Le libellé de la Tournée.
+     * @param producteur Le Producteur associé à la Tournée.
+     * @param vehicule   Le Véhicule associé à la Tournée.
+     * @param commandes  La liste de Commandes composant la Tournée.
+     * @param poids      Le poids de la Tournée
      */
     public FormAddTourValidator(String libelle, Producteur producteur, Vehicule vehicule, ArrayList<Commande> commandes,
             String poids) {
@@ -28,16 +31,17 @@ public class FormAddTourValidator extends FormTourValidator {
         // On affiche les erreurs avant de faire d'autres verifications.
         if (!isValid()) {
             return;
-        }
+        } // if
 
         if ( !ValidateurTournee.valideVehicule(vehicule, horaires[0], horaires[1])) {
-            setInvalid("Le véhicule n'est pas disponible pour ce créneau horaire !");
-        }
+            setInvalid("Le Véhicule n'est pas disponible pour ce créneau horaire !");
+        } // if
 
-        // On vérifie que la commande n'est pas deja dans une tournee.
+        // On vérifie que la Commande n'est pas deja dans une Tournée.
         Optional<Commande> cmd = commandes.stream().filter(commande -> commande.getTournee() != null).findFirst();
         cmd.ifPresent(
-                commande -> setInvalid("La commande " + commande.getLibelle() + " est déja dans une tournée."));
+                commande -> setInvalid("La Commande " + commande.getLibelle() + " est déja dans une Tournée."));
 
-    }
-}
+    } // constructeur
+
+} // FormAddTourValidator
