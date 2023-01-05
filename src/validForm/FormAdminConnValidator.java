7f95276f8ee8ed55334a823037c3c0c92039a5e2
Pipoune <GnQ@outlook.fr>
2023-01-05 15:56:31 +0100

    Correction orthographique et conventions de codage - FormAdminConnValidator

--- src/validForm/FormAdminConnValidator.java
+++ src/validForm/FormAdminConnValidator.java
@@ -10,17 +10,20 @@ import utility.DatabaseConnection;
 import utility.UserAuth;
 
 /**
+ * Validateur de formulaire pour AdminConnCtrl.
  * Permet d'authentifier un administrateur.
  * Ce formulaire a comme particularité d'authentifier l'utilisateur dans l'objet session UserAuth.
  * @see UserAuth
  * @see controllers.AdminConnCtrl
  */
 public class FormAdminConnValidator extends FormValidator {
+
     /**
+     * Constructeur de FormAdminConnValidator.
      * Vérifie que le mot de passe fourni correspond bien au mot de passe en base,
      * Ce mot de passe est hashé avec Argon2.
-     * @param login le pseudonyme de l'admin
-     * @param password le mot de passe
+     * @param login Le pseudonyme de l'Administrateur
+     * @param password Le mot de passe
      */
     public FormAdminConnValidator(String login, String password) {
         AdministrateurDAO aDAO;
@@ -30,21 +33,22 @@ public class FormAdminConnValidator extends FormValidator {
         } catch (ClassNotFoundException | SQLException | IOException e) {
             setInvalid(e.toString());
             return;
-        }
+        } // try/catch
 
         Administrateur adm = aDAO.get(login);
 
         if (adm == null) {
             setInvalid("Utilisateur non inscrit.");
             return;
-        }
+        } // if
 
         if (!Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id, 32, 64).verify(adm.getMdpAdmin(),
                 password.toCharArray())) {
             setInvalid("Mot de passe invalide.");
             return;
-        }
+        } // if
 
         UserAuth.setAdministrateur(adm);
-    }
-}
+    } // constructeur
+
+} // FormAdminConnValidator
