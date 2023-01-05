7f0ae26d0400e77e4aba4b6dd9773f9b42592833
Pipoune <GnQ@outlook.fr>
2023-01-05 08:44:45 +0100

    Correction orthographique et conventions de codage - ValidateurAdresseTest

--- src/tests/validators/ValidateurAdresseTest.java
+++ src/tests/validators/ValidateurAdresseTest.java
@@ -17,29 +17,59 @@ public class ValidateurAdresseTest {
     public void adresseCreationTest() throws AdresseInvalideException {
 
         assertThrows(AdresseInvalideException.class, () -> {
-            ValidateurAdresse.create("8", "Boulevard", "machin", "Amiens", "80000");
-        });
-        ValidateurAdresse adresse = ValidateurAdresse.create("8", "Boulevard", "du Port", "Amiens", "80000");
+            ValidateurAdresse.create(
+                "8",
+                "Boulevard",
+                "machin",
+                "Amiens",
+                "80000"
+            ); // create
+        }); // assertThrows
+        ValidateurAdresse adresse = ValidateurAdresse.create(
+            "8",
+            "Boulevard",
+            "du Port",
+            "Amiens",
+            "80000"
+            ); // create
         assertEquals(adresse.getCoordX(), "49.897442");
         assertEquals(adresse.getCoordY(), "2.290084");
         assertEquals(adresse.getCoordXY(), "2.290084,49.897442");
-    }
+    } // adresseCreationTest
 
     /**
-     * Permet de vérifier que demander une adresse déjà vérifiée renvoie le meme objet.
+     * Permet de vérifier que demander une adresse déjà vérifiée renvoie le même objet.
      */
     @Test
-    @DisplayName("Renvoie d'une adresse existante")
+    @DisplayName("Renvoi d'une adresse existante")
     public void adresseExistanteTest() throws AdresseInvalideException {
-        ValidateurAdresse adresse = ValidateurAdresse.create("8", "Boulevard", "du Port", "Amiens", "80000");
-        ValidateurAdresse adresseExistante = ValidateurAdresse.create("8", "Boulevard", "du Port", "Amiens", "80000");
-        ValidateurAdresse adressPresqueIdentique = ValidateurAdresse.create("3", "Boulevard", "du Port", "Amiens", "80000");
+        ValidateurAdresse adresse = ValidateurAdresse.create(
+            "8",
+            "Boulevard",
+            "du Port",
+            "Amiens",
+            "80000"
+            ); // create
+        ValidateurAdresse adresseExistante = ValidateurAdresse.create(
+            "8",
+            "Boulevard",
+            "du Port",
+            "Amiens",
+            "80000"
+            ); // create
+        ValidateurAdresse adressePresqueIdentique = ValidateurAdresse.create(
+            "3",
+            "Boulevard",
+            "du Port",
+            "Amiens",
+            "80000"
+            ); // create
         assertEquals(adresse.getCoordX(), "49.897442");
         assertEquals(adresse.getCoordY(), "2.290084");
         assertEquals(adresse.getCoordXY(), "2.290084,49.897442");
         assertEquals(adresse, adresseExistante);
-        assertNotEquals(adressPresqueIdentique, adresseExistante);
-    }
+        assertNotEquals(adressePresqueIdentique, adresseExistante);
+    } // adresseExistanteTest
 
     /**
      * Permet de vérifier le format d'une adresse.
@@ -47,7 +77,14 @@ public class ValidateurAdresseTest {
     @Test
     @DisplayName("Validation du format d'une adresse")
     public void formatTest() throws AdresseInvalideException {
-        ValidateurAdresse adresse = ValidateurAdresse.create("8", "Boulevard", "du Port", "Amiens", "80000");
+        ValidateurAdresse adresse = ValidateurAdresse.create(
+            "8",
+            "Boulevard",
+            "du Port",
+            "Amiens",
+            "80000"
+            ); // create
         assertEquals(adresse.format(), "8 Boulevard du Port Amiens 80000");
-    }
-}
+    } // formatTest
+
+} // ValidateurAdresseTest
