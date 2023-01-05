a424a12b5757bdb6264f5dde34fbecca39804ebf
Pipoune <GnQ@outlook.fr>
2023-01-05 04:16:24 +0100

    Correction orthographique et conventions de codage - DeleteCommCtrl

--- src/controllers/DeleteCommCtrl.java
+++ src/controllers/DeleteCommCtrl.java
@@ -27,10 +27,10 @@ public class DeleteCommCtrl extends AbstractConnCtrl implements Initializable {
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
         commLabelText.setText(commande.getLibelle());
-    }
+    } // initialize
 
     /**
-     * Méthode qui valide la suppression de la commande.
+     * Méthode qui valide la suppression de la Commande.
      * 
      * @param event ActionEvent
      */
@@ -40,28 +40,30 @@ public class DeleteCommCtrl extends AbstractConnCtrl implements Initializable {
         if (formulaire.isValid()) {
             commDAO.delete(commande);
             ControllersUtils.closePopupAndUpdateParent(event);
-        } else {
+        } // if
+        else {
             deleteErrorText.setVisible(true);
             deleteErrorText.setText(formulaire.getErrors());
-        }
-    }
+        } // else
+    } // validateDeleteComm
 
     /**
-     * Méthode qui annule la suppression de la commande.
+     * Méthode qui annule la suppression de la Commande.
      * 
      * @param event ActionEvent
      */
     public void cancelDeleteComm(ActionEvent event) {
         ControllersUtils.closePopupAndUpdateParent(event);
-    }
+    } // cancelDeleteComm
 
     /**
-     * Méthode qui récupère la commande sélectionnée dans la listView
+     * Méthode qui récupère la Commande sélectionnée dans la listView
      * de la vue précédente (prodSelectMenu)
      * 
      * @param comm Commande
      */
     public static void setCommande(Commande comm) {
         commande = comm;
-    }
-}
+    } // setCommande
+
+} // DeleteCommCtrl
