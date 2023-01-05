9240527efdc7bc4232934ae3168589a992c1cf3f
Pipoune <GnQ@outlook.fr>
2023-01-05 15:59:15 +0100

    Correction orthographique et conventions de codage - FormTourValidator

--- src/validForm/FormTourValidator.java
+++ src/validForm/FormTourValidator.java
@@ -11,53 +11,58 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 
 /**
- * Formulaire d'ajout de tournée.
+ * Validateur de formulaire pour AddTourCtrl & ModifyTourCtrl.
+ *
+ * @see controllers.AddTourCtrl
+ * @see controllers.ModifyTourCtrl
  */
 public class FormTourValidator extends FormValidator {
     protected Timestamp[] horaires;
 
     /**
+     * Constructeur de FormTourValidator
      *
-     * @param libelle    le libellé pour identifier la tournée.
-     * @param producteur le producteur associé à la tournée.
-     * @param vehicule   le vehicle associé à la tournée.
-     * @param commandes  la liste de commande composant la tournée.
-     * @param poids      le poids de la tournée
+     * @param libelle    Le libellé de la Tournée récupéré dans la vue.
+     * @param producteur Le Producteur associé à la Tournée récupéré dans la vue.
+     * @param vehicule   Le Véhicule associé à la Tournée récupéré dans la vue.
+     * @param commandes  La liste de Commandes composant la Tournée récupérée dans la vue.
+     * @param poids      Le poids de la Tournée récupéré dans la vue.
      */
     public FormTourValidator(String libelle, Producteur producteur, Vehicule vehicule, ArrayList<Commande> commandes,
             String poids) {
         if (libelle.isEmpty()) {
-            setInvalid("Veuillez écrire un libellé !");
+            setInvalid("Veuillez entrer un libellé");
             return;
-        }
+        } // if
         if (vehicule == null) {
-            setInvalid("Veuillez choisir un véhicule !");
+            setInvalid("Veuillez choisir un Véhicule");
             return;
-        }
+        } // if
         if (commandes.isEmpty()) {
-            setInvalid("Veuillez choisir une commande !");
+            setInvalid("Veuillez choisir une Commande minimum");
             return;
-        }
+        } // if
         if(commandes.size() > 9) {
-            setInvalid("Une tournée doit contenir moins de 10 commandes !");
+            setInvalid("Une Tournée ne peut pas contenir plus de 9 Commandes");
             return;
-        }
+        } // if
         if (!ValidateurTournee.validePoids(vehicule.getPoidsMax(), commandes)) {
-            setInvalid("La capacité du véhicule est dépassé !");
-        }
+            setInvalid("La capacité du Véhicule est dépassée");
+        } // if
 
         try {
             horaires = ValidateurTournee.calculTournee(commandes, producteur.getGpsProd());
         } catch (IOException | InterruptedException | InvalidRouteException e) {
-            setInvalid("La suite de commandes est incorrecte !");
-        }
-    }
+            setInvalid("La Tournée n'est pas valide");
+        } // try/catch
+    } // constructeur
 
     public Timestamp getHeureDebut() {
         return horaires[0];
-    }
+    } // getHeureDebut
 
     public Timestamp getHeureFin() {
         return horaires[1];
-    }
-}
+    } // getHeureFin
+
+} // FormTourValidator
