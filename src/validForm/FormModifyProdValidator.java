647c8f3afa5ba2e96c97f7da5e998088dd6c87f1
Pipoune <GnQ@outlook.fr>
2023-01-05 15:58:09 +0100

    Correction orthographique et conventions de codage - FormModifyProdValidator

--- src/validForm/FormModifyProdValidator.java
+++ src/validForm/FormModifyProdValidator.java
@@ -6,7 +6,7 @@ import validator.ValidateurAdresse;
 import validator.ValidateurDonnee;
 
 /**
- * Utilisé lorsqu'on modifie un Producteur.
+ * Validateur de formulaire pour ModifyProdCtrl.
  * 
  * @see controllers.ModifyProdCtrl
  */
@@ -15,17 +15,17 @@ public class FormModifyProdValidator extends FormProdValidator {
     /**
      * Constructeur de FormModifyProdValidator.
      *
-     * @param siret           le siret du producteur.
-     * @param proprietaire    le nom du producteur.
-     * @param addressNumField le numéro de rue du producteur.
-     * @param addressPathType le type de voie dans laquelle travaille le producteur.
-     * @param addressPathName le type de voie du Producteur récupéré dans la vue
-     * @param addressTownName la ville du Producteur.
-     * @param addressPostCode le code Postal du producteur
-     * @param numTelProd      le numéro de téléphone du producteur.
-     * @param password        le mot de passe du producteur
-     * @param confirmPassword le mot de passe à confirmer.
-     * @param producteur      le producteur.
+     * @param siret           Le SIRET du Producteur.
+     * @param proprietaire    Le nom du Producteur.
+     * @param addressNumField Le numéro de rue du Producteur.
+     * @param addressPathType Le type de voie dans laquelle travaille le Producteur.
+     * @param addressPathName Le type de voie du Producteur récupéré dans la vue
+     * @param addressTownName La ville du Producteur.
+     * @param addressPostCode Le code postal du Producteur
+     * @param numTelProd      Le numéro de téléphone du Producteur.
+     * @param password        Le mot de passe du Producteur
+     * @param confirmPassword Le mot de passe à confirmer.
+     * @param producteur      Le Producteur.
      */
     public FormModifyProdValidator(String siret, String proprietaire, String addressNumField, String addressPathType,
             String addressPathName, String addressTownName, String addressPostCode, String numTelProd, String password,
@@ -35,35 +35,35 @@ public class FormModifyProdValidator extends FormProdValidator {
 
         if (!isValid()) {
             return;
-        }
+        } // if
 
-        // Si le mot de passe est vide, il reste le meme.
+        // Si le mot de passe est vide, il reste le même.
         if (!password.isEmpty()) {
             if (!ValidateurDonnee.validePassword(password)) {
                 setInvalid(
                         "Le mot de passe ne répond pas aux exigences minimales de sécurité :\n9 caractères\n1 caractère spécial\n1 majuscule\n1 minuscule\n1 chiffre");
-            }
-        }
+            } // if
+        } // if
 
         ValidateurAdresse adresseValide;
         try {
             adresseValide = ValidateurAdresse.create(addressNumField, addressPathType, addressPathName, addressTownName,
                     addressPostCode);
 
-            // si on veut changer l'adresse
+            // Si on veut changer l'adresse
             if (!adresseValide.csv().equals(producteur.getAdresseProd()) && !producteur.getTournees().isEmpty()) {
                 System.out.println(adresseValide.csv());
                 System.out.println(producteur.getAdresseProd());
-                setInvalid("Impossible de modifier l'adresse car le producteur est concerne par une tournée.");
+                setInvalid("Impossible de modifier l'adresse car le Producteur est concerné par une Tournée.");
                 return;
-            }
+            } // if
         } catch (NumberFormatException | AdresseInvalideException e) {
             setInvalid("L'adresse n'est pas valide");
             return;
-        }
+        } // try/catch
 
         setAdresseCsv(adresseValide.csv());
         setCoordXY(adresseValide.getCoordXY());
+    } // constructeur
 
-    }
-}
+} // FormModifyProdValidator
